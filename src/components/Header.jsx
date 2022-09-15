cc1205f4d64c3215a69be11ab444861686fb2b04
alexisMJI <alexismarin11@hotmail.es>
2022-09-15 01:57:41 -0300

    f4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
     <header className='px-4 py-5 bg-white border-b'>
       <div className='md:flex md: justify-between'>
         <h2 className='text-4xl text-sky-600 font-black text-center'>
-          Uptask
+          Recetario
         </h2>
         {/*<input type="search" placeholder='Buscar Recetas' className='rounded-lg lg:w96 block p-2 border' />*/}
           
